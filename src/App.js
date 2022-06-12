1e1ded86e2d44cf7d78252707a5cbe1d83d04cc2
atifsalim <atifsalimbhat@gmail.com>
2022-06-12 16:40:35 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, {useRef,useState} from "react";
+
+import Records from './records.json';
 import './App.css';
 
 function App() {
+
+const nameInput= useRef(null);
+
+const [btn,setBtn]=useState('Subscribe');
+
+const [followBtn,setfollowBtn]=useState('Follow');
+
+const change=()=>{
+  setBtn('Subscribed');
+
+
+}
+
+
+const changeFollow=(props)=>{
+  console.log(props.key);
+  
+  setfollowBtn('Followed');
+
+
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{color:"blue"}}>List of  Ongoing Projects</h1>
+      <hr/>
+
+      {
+        Records.map((record,idx) => {
+          return (
+            <div className="box" key={record.id}>
+              <h2 style={{color:"green", fontSize:"30px"}}>{record.title} <br/></h2>
+              <h4>{record.description} </h4>
+              <button onClick={change}  ref={nameInput}
+               style={{color:"red", fontSize:"30px"}}>{btn}</button>
+
+              <button onClick={changeFollow}  key={idx}
+               style={{color:"red", fontSize:"30px"}}>{followBtn}</button>
+
+
+              <hr/> 
+              
+              </div>
+              
+          )
+        })
+      }
+
+
+
     </div>
   );
 }
